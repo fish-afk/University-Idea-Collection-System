48f7676147ac3b60df7591520f65bab1ee2dd4ed
fish-afk <mirzashihab2@gmail.com>
2023-11-05 20:02:48 +0200

    merge done

--- Idea_collection_system/static/js/script.js
+++ Idea_collection_system/static/js/script.js
@@ -1,3 +1,14 @@
+//Hamburger menu on mobile devices
+
+function myFunction() {
+	var x = document.getElementById("nav");
+	if (x.className === "navbar") {
+		x.className += " responsive";
+	} else {
+		x.className = "navbar";
+	}
+}
+
 //enable submit idea button when user agrees to terms and conditions
 function enable() {
 	var check = document.getElementById("check");
@@ -12,48 +23,56 @@ function enable() {
 
 //like animations
 function like() {
-	const like = document.getElementById("like");
+	const like = document.querySelectorAll(".like");
 
-	like.addEventListener("click", () => {
-		if (like.classList.contains("far")) {
-			// If not solid, switch it to solid
-			like.classList.remove("far");
-			like.classList.add("fas");
-		} else {
-			// If solid, switch it to not solid
-			like.classList.remove("fas");
-			like.classList.add("far");
-		}
+	like.forEach((like) => {
+		like.addEventListener("click", () => {
+			if (like.classList.contains("far")) {
+				// If not solid, switch it to solid
+				like.classList.remove("far");
+				like.classList.add("fas");
+			} else {
+				// If solid, switch it to not solid
+				like.classList.remove("fas");
+				like.classList.add("far");
+			}
+		});
 	});
 }
 
 function dislike() {
-	const dislike = document.getElementById("dislike");
+	const dislike = document.querySelectorAll(".dislike");
 
-	dislike.addEventListener("click", () => {
-		if (dislike.classList.contains("far")) {
-			// If not solid, switch it to solid
-			dislike.classList.remove("far");
-			dislike.classList.add("fas");
-		} else {
-			// If solid, switch it to not solid
-			dislike.classList.remove("fas");
-			dislike.classList.add("far");
-		}
+	dislike.forEach((dislike) => {
+		dislike.addEventListener("click", () => {
+			if (dislike.classList.contains("far")) {
+				// If not solid, switch it to solid
+				dislike.classList.remove("far");
+				dislike.classList.add("fas");
+			} else {
+				// If solid, switch it to not solid
+				dislike.classList.remove("fas");
+				dislike.classList.add("far");
+			}
+		});
 	});
 }
 
 function comments() {
-	const icon = document.getElementById("commenticon");
+	const icons = document.querySelectorAll(".comment-icon");
+	const commentSections = document.querySelectorAll(".comments");
 
-	const comments = document.getElementById("comments");
-
-	icon.addEventListener("click", () => {
-		// Toggle the visibility of the div
-		if (comments.style.display === "none") {
-			comments.style.display = "block";
-		} else {
-			comments.style.display = "none";
-		}
+	icons.forEach((icon, index) => {
+		icon.addEventListener("click", () => {
+			// Toggle the visibility of the corresponding comment section
+			if (
+				commentSections[index].style.display === "none" ||
+				commentSections[index].style.display === ""
+			) {
+				commentSections[index].style.display = "block";
+			} else {
+				commentSections[index].style.display = "none";
+			}
+		});
 	});
 }
